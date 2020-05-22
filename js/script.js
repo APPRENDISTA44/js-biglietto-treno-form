27dd4d4c6f6a6288ab9e23350739e39693489fa6
Luca Isidoro <apprendista44@gmail.com>
2020-05-22 17:04:25 +0200

    aggiunti dettagli e aggiunto errore con dati sbagliati

--- js/script.js
+++ js/script.js
@@ -16,29 +16,38 @@ var biglCosto = document.getElementById('bigl-costo');
 var formKmValue;
 var formEtaValue;
 var visibilita = document.getElementById('visibilita');
+var errore = document.getElementById('errore');
 
 //evento click su genera
 formGenera.addEventListener('click',
   function(){
     formKmValue = formKm.value;
     formEtaValue = formEta.value;
-    
-    //calcolo
-    var prezzo = 0.21 * formKmValue;
-    var offerta = 'Prezzo standard'
-    if (formEtaValue == 'minorenne') {
-      prezzo = prezzo - (prezzo/100)*20;
-      offerta = 'Sconto Silver'
-    } else if (formEtaValue == 'over') {
-      prezzo = prezzo - (prezzo/100)*40;
-      offerta = 'Sconto Gold'
+    forNameValue = formName.value;
+    //se clicca su genera senza avere annullato prima scompare la scritta di errore
+    errore.className = 'hidden'
+    //controllo dati utente
+    if ((isNaN(formKmValue)) || !(isNaN(forNameValue)) || (formKmValue == '')) {
+      errore.className = 'visible'
+    }else {
+      //calcolo
+      var prezzo = 0.21 * formKmValue;
+      var offerta = 'Prezzo standard'
+      if (formEtaValue == 'minorenne') {
+        prezzo = prezzo - (prezzo/100)*20;
+        offerta = 'Sconto Silver'
+      } else if (formEtaValue == 'over') {
+        prezzo = prezzo - (prezzo/100)*40;
+        offerta = 'Sconto Gold'
+      }
+      biglNome.innerHTML = formName.value;
+      biglOfferta.innerHTML = offerta;
+      biglCarrozza.innerHTML = Math.floor(Math.random()*10)+1;
+      biglCp.innerHTML = Math.floor(Math.random()*100000)+1; ;
+      biglCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
+      visibilita.className = 'visible'
     }
-    biglNome.innerHTML = formName.value;
-    biglOfferta.innerHTML = offerta;
-    biglCarrozza.innerHTML = Math.floor(Math.random()*10)+1;
-    biglCp.innerHTML = Math.floor(Math.random()*100000)+1; ;
-    biglCosto.innerHTML = prezzo.toFixed(2) + ' Euro';
-    visibilita.className = 'visible'
+
   }
 );
 
@@ -49,5 +58,6 @@ formAnnulla.addEventListener('click',
   formKm.value = '';
   formEta.value = 'maggiorenne'
   visibilita.className = 'hidden'
+  errore.className = 'hidden'
   }
 );
